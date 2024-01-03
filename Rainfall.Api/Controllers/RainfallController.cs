4ed24cab6f36c44ae2eb6aac73ba93e762f65f63
rutherson <rutherson20@gmail.com>
2024-01-03 23:23:07 +0800

    add validation and return for rainfallcontroller

--- Rainfall.Api/Controllers/RainfallController.cs
+++ Rainfall.Api/Controllers/RainfallController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Rainfall.Api.Client;
+using Rainfall.Api.Models;
 
 namespace Rainfall.Api.Controllers
 {
@@ -16,11 +17,59 @@ namespace Rainfall.Api.Controllers
 
         [HttpGet]
         [Route("id/{stationId}/readings")]
-        public async Task<IActionResult> Get(string stationId, int count = 100)
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(RainfallReadingResponse))]
+        [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(ErrorResponse))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(ErrorResponse))]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(ErrorResponse))]
+        public async Task<IActionResult> Get(string stationId, int count = 10)
         {
-            var measures = await _stationMeasuresClient.GetStationMeasures(stationId, count);
+            //Validate
+            if (count > 100 || count < 1 || string.IsNullOrEmpty(stationId))
+            {
+                var errorDetails = new List<ErrorDetail>();
+                if (string.IsNullOrEmpty(stationId))
+                    errorDetails.Add(new ErrorDetail { PropertyName = "stationId", Message = "StationId is Required" });
 
-            return measures == null ? NotFound() : Ok(measures);
+                if (count > 100 || count < 1)
+                    errorDetails.Add(new ErrorDetail { PropertyName = "count", Message = "Must be minimum of 1 and maximum of 100" });
+
+                return BadRequest(new ErrorResponse
+                {
+                    Message = "Invalid request",
+                    Details = errorDetails
+                });
+            }
+
+            //Get Measures
+            try
+            {
+                var readings = await _stationMeasuresClient.GetStationMeasures(stationId, count);
+
+                if (readings != null)
+                {
+                    if (readings.RainfallReadings.Count > 0)
+                        return Ok(readings);
+                    else
+                        return NotFound(new ErrorResponse
+                        {
+                            Message = "No readings found for the specified stationId"
+                        });
+                }
+                else
+                {
+                    return StatusCode(StatusCodes.Status500InternalServerError, new ErrorResponse
+                    {
+                        Message = "Internal Server Error"
+                    });
+                }
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new ErrorResponse
+                {
+                    Message = "Internal Server Error"
+                });
+            }
         }
     }
 }
